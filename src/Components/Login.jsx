582a5513df26dff55132d40d48d27b1923c4ad96
bonghee <hae8064@naver.com>
2022-12-16 00:31:39 +0900

    라우터 적용해서 /일때 로그인 페이지 라우팅 완료, 로그인 페이지 ui 꾸미는 중

---
+++ src/Components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="loginComponent">
+      <div className="loginTitle">로그인</div>
+    </div>
+  );
+};
+
+export default Login;
