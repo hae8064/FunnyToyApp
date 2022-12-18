79afe3fee2329293c2bf6b2db4ba762397a01512
bonghee <hae8064@naver.com>
2022-12-18 20:14:38 +0900

    로그인 유효성 검사 진행 중 이메일 진행 완료

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,13 +1,60 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignUp.css';
 
 const SignUp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [pwd2, setPwd2] = useState('');
+
+  const [emailPass, setEmailPass] = useState(false);
+  //이름 state 저장
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  //이메일 유효성
+  const isEmail = (email) => {
+    const emailRegex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+    return emailRegex.test(email);
+  };
+
+  const onEmailChange = (e) => {
+    setEmailPass(true);
+    setEmail(e.target.value);
+  };
+
+  useEffect(() => {
+    if (isEmail(email)) {
+      setEmail(email);
+      setEmailPass(false);
+    } else {
+      setEmail('');
+    }
+  }, [name, email, pwd, pwd2]);
   return (
     <div className="signupComponent">
       <div className="signUpTitle">회원가입</div>
-      <input type="text" className="nameInput" placeholder="이름" />
-      <input type="text" className="signUpEmailInput" placeholder="이메일" />
+      <input
+        type="text"
+        className="nameInput"
+        placeholder="이름"
+        onChange={onNameChange}
+      />
+      <input
+        type="text"
+        className="signUpEmailInput"
+        placeholder="이메일"
+        onChange={onEmailChange}
+      />
+      <div className={`emailCheckText${emailPass}`}>
+        이메일 형식이 아닙니다.
+      </div>
       <input type="password" className="passwordInput" placeholder="비밀번호" />
       <input
         type="password"
