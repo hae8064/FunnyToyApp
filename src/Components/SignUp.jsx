5e125af6a3f728b16544ece6d2d9aa64573ca797
bonghee <hae8064@naver.com>
2022-12-18 21:45:09 +0900

    회원가입 유효성 전부 완료

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -10,7 +10,11 @@ const SignUp = () => {
   const [pwd, setPwd] = useState('');
   const [pwd2, setPwd2] = useState('');
 
-  const [emailPass, setEmailPass] = useState(false);
+  const [emailPass, setEmailPass] = useState(true);
+  const [pwdPass, setPwdPass] = useState(true);
+  const [pwd2Pass, setPwd2Pass] = useState(true);
+  const [signPass, setSignPass] = useState(true);
+
   //이름 state 저장
   const onNameChange = (e) => {
     setName(e.target.value);
@@ -24,19 +28,66 @@ const SignUp = () => {
     return emailRegex.test(email);
   };
 
+  //비밀번호 유효성 (숫자+영문자+특수문자 포함 8자리 이상)
+  const isPassword = (pwd) => {
+    const passwordRegex =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+
+    return passwordRegex.test(pwd);
+  };
+
   const onEmailChange = (e) => {
-    setEmailPass(true);
+    setEmailPass(false);
     setEmail(e.target.value);
+  };
+
+  const onPwdChange = (e) => {
+    setPwd(e.target.value);
+    setPwdPass(false);
+  };
+
+  const onPwd2Change = (e) => {
+    setPwd2(e.target.value);
+    setPwd2Pass(false);
+  };
+
+  const onSignUpButton = () => {
+    if (
+      pwdPass === true &&
+      pwd2Pass === true &&
+      emailPass === true &&
+      email !== '' &&
+      pwd !== '' &&
+      pwd2 !== ''
+    ) {
+      console.log('회원가입  성공!');
+      setSignPass(true);
+    } else {
+      setSignPass(false);
+    }
   };
 
   useEffect(() => {
     if (isEmail(email)) {
       setEmail(email);
-      setEmailPass(false);
+      setEmailPass(true);
     } else {
       setEmail('');
     }
+    if (isPassword(pwd)) {
+      setPwd(pwd);
+      setPwdPass(true);
+    } else {
+      setPwd('');
+    }
+    if (pwd === pwd2) {
+      setPwd2Pass(true);
+    } else {
+      setPwd2('');
+      setPwd2Pass(false);
+    }
   }, [name, email, pwd, pwd2]);
+
   return (
     <div className="signupComponent">
       <div className="signUpTitle">회원가입</div>
@@ -55,13 +106,30 @@ const SignUp = () => {
       <div className={`emailCheckText${emailPass}`}>
         이메일 형식이 아닙니다.
       </div>
-      <input type="password" className="passwordInput" placeholder="비밀번호" />
+      <input
+        type="password"
+        className="passwordInput"
+        placeholder="비밀번호"
+        onChange={onPwdChange}
+      />
+      <div className={`pwdCheckText${pwdPass}`}>
+        숫자+영문자+특수문자 포함 8자리 이상 입력해주세요.
+      </div>
       <input
         type="password"
         className="passwordInput2"
         placeholder="비밀번호 확인"
+        onChange={onPwd2Change}
       />
-      <button className="signUpButton">회원가입</button>
+      <div className={`pwd2CheckText${pwd2Pass}`}>
+        비밀번호가 일치하지 않습니다.
+      </div>
+      <button className="signUpButton" onClick={onSignUpButton}>
+        회원가입
+      </button>
+      <div className={`signCheckText${signPass}`}>
+        회원가입에 실패하였습니다.
+      </div>
       <Link to="/" style={{ textDecoration: 'none', color: 'grey' }}>
         <div className="signInText">이미 가입된 Email이 있으신가요?</div>
       </Link>
