c12bf58aa98843a61678157e4fa96fabb4f9c5ad
bonghee <hae8064@naver.com>
2022-12-27 16:41:40 +0900

    메모 예제 리스트 생성 완료

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 import './Home.css';
 
 const Home = () => {
+  const [memo, setMemo] = useState(0);
+
   return (
     <div className="homeComponent">
       <div className="homeTop">
@@ -11,6 +14,17 @@ const Home = () => {
         </div>
         <div className="rightFetures">
           <button className="homeDeleteButton">삭제</button>
+        </div>
+      </div>
+      <div className="homeBody">
+        <div className="memoContent">
+          <div className="memoContentLeft">
+            <input type="checkbox" />
+          </div>
+          <div className="memoContentRight">
+            <div className="memotitle">테스트 맛집 제목</div>
+            <div className="momoDate">테스트 맛집 방문 날짜</div>
+          </div>
         </div>
       </div>
     </div>
