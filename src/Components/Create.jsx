47906e6be6f25b59fa6c6d7da93fd16bec1d8219
bonghee <hae8064@naver.com>
2022-12-28 14:13:37 +0900

    네이버 맵 api 위치 추가 테스트 위도 경도를 활용한 주소 등록

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,13 +1,40 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Create.css';
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps';
 
 const Create = () => {
+  const { naver } = window;
   const stars = [1, 2, 3, 4, 5];
   const [starColor, setStarColor] = useState('grey');
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   const [currentStar, setCurrentStar] = useState(0);
   const [imgFile, setImgFile] = useState('');
+  const [location, setLocation] = useState([]);
   const imgRef = useRef();
+
+  // useEffect(() => {
+  //   naver.maps.Service.reverseGeocode(
+  //     {
+  //       location: new naver.maps.LatLng(
+  //         // lat === undefined ? 37.3849483 : lat2,
+  //         // lng === undefined ? 127.1229117 : lng2
+  //         37.3849483,
+  //         127.1229117
+  //       ),
+  //     },
+  //     function (status, response) {
+  //       if (status !== naver.maps.Service.Status.OK) {
+  //         return alert('Something Wrong!');
+  //       }
+
+  //       const result = response.result;
+  //       setLocation(result.items[0].address);
+  //     }
+  //   );
+  // }, []);
 
   const starClick = (e) => {
     setCurrentStar(e);
@@ -23,6 +50,34 @@ const Create = () => {
       setImgFile(reader.result);
     };
   };
+
+  //위치 등록 버튼
+  const onLocationButton = () => {
+    naver.maps.Service.reverseGeocode(
+      {
+        location: new naver.maps.LatLng(
+          // lat === undefined ? 37.3849483 : lat2,
+          // lng === undefined ? 127.1229117 : lng2
+          37.3849483,
+          127.1229117
+        ),
+      },
+      function (status, response) {
+        if (status !== naver.maps.Service.Status.OK) {
+          return alert('Something Wrong!');
+        }
+
+        const result = response.result;
+        setLocation(result.items[0].address);
+      }
+    );
+  };
+
+  //저장 버튼 클릭 이벤트
+  const onCreateButton = () => {
+    //제목, 내용, 점수를 입력 안했으면 생성 불가능
+  };
+
   return (
     <div className="createComponent">
       <div className="createDelecious">맛집 기록</div>
@@ -64,8 +119,12 @@ const Create = () => {
           ))}
         </div>
         <div className="createLocation">
-          <button className="createLocationButton">위치등록</button>
-          <div className="createLocationText"></div>
+          <button className="createLocationButton" onClick={onLocationButton}>
+            위치등록
+          </button>
+          <div className="createLocationText">
+            {location.length === 0 ? '등록된 위치 없음' : location}
+          </div>
         </div>
         <div className="createInput">
           <label className="createLabelInput" for="createFileImg">
@@ -81,7 +140,9 @@ const Create = () => {
           <img className="createImg" src={imgFile ?? imgFile} />
         </div>
         <div className="createBottom">
-          <button className="createCancelButton">취소</button>
+          <Link to="/home">
+            <button className="createCancelButton">취소</button>
+          </Link>
           <button className="createSaveButton">저장</button>
         </div>
       </div>
