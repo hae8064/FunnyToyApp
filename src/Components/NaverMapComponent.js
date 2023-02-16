436c9a5684381fc23a1a20737408d308eac73c19
bonghee <hae8064@naver.com>
2023-02-16 23:53:25 +0900

    [add] 현재 위, 경도를 받아서 주소 출력 기능 추가

--- src/Components/NaverMapComponent.js
+++ src/Components/NaverMapComponent.js
@@ -21,7 +21,7 @@ const NaverMapComponent = () => {
     } else {
       window.alert('현재위치를 알수 없습니다.');
     }
-  }, [myLocation]);
+  }, []);
 
   useEffect(() => {
     // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
@@ -40,7 +40,11 @@ const NaverMapComponent = () => {
       map,
       position: location,
     });
-  }, []);
+
+    dispatch(
+      currentSet({ lat: myLocation.latitude, lng: myLocation.longitude })
+    );
+  }, [myLocation]);
   return <div id="map" style={{ width: '100%', height: '350px' }} />;
 };
 
--- src/Pages/Map.jsx
+++ src/Pages/Map.jsx
@@ -4,6 +4,7 @@ import NaverMapComponent from '../Components/NaverMapComponent';
 import { Inner } from './styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { currentSet } from '../store/locationSlice';
+import useGeolocation from '../hooks/useGeolocation.ts';
 
 const Map = () => {
   //내 위치 가져오기 (리덕스 툴킷 사용)
@@ -11,18 +12,44 @@ const Map = () => {
   const locationState = useSelector((state) => {
     return state.currentLocation.value;
   });
+  const { naver } = window;
+  const [currentLocation, setCurrentLocation] = useState([]);
+  const naverLocation = useGeolocation();
+
+  const onRefreshBtn = () => {
+    console.log(naverLocation.coordinates.lat.toFixed(4));
+
+    naver.maps.Service.reverseGeocode(
+      {
+        location: new naver.maps.LatLng(
+          // lat === undefined ? 37.3849483 : lat2,
+          // lng === undefined ? 127.1229117 : lng2
+          naverLocation.coordinates.lat.toFixed(4),
+          naverLocation.coordinates.lng.toFixed(4)
+        ),
+        orders: [
+          naver.maps.Service.OrderType.ADDR,
+          naver.maps.Service.OrderType.ROAD_ADDR,
+        ].join(','),
+      },
+      function (status, response) {
+        const result = response;
+        setCurrentLocation(result.result.items[0].address);
+      }
+    );
+  };
 
   // 처음 렌더링시 서버로 API 요청
   // 서버에서 네이버 API 요청하기..
-
   useEffect(() => {
     // dispatch(currentSet({ dada: 1, dsa: 2 }));
-    console.log(locationState);
-  }, [locationState]);
+    console.log('현재 주소: ' + currentLocation);
+  }, [currentLocation]);
 
   return (
     <Inner>
       <div className="Title">주변 맛집 리스트</div>
+      <button onClick={onRefreshBtn}>내 주변 조회</button>
       <NaverMapComponent />
     </Inner>
   );
